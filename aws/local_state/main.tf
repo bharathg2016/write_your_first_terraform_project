3511f427ddd99a61b55d01ae29a00057686ef044
Bharathkumarg <101990136+bharathg2016@users.noreply.github.com>
2023-07-23 00:52:09 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "web" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
 
